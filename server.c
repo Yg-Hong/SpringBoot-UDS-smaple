5c9b21f047d9076cc657706fab8113620212793a
Yg-Hong <lion0077v@gmail.com>
2024-08-07 12:42:18 +0900

    test

--- server.c
+++ server.c
@@ -3,15 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <time.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
 
 #define SOCKET_PATH "/tmp/my_unix_socket1.sock"
-#define BUFFER_SIZE         2048
-#define BACKLOG             5
-#define SELECT_TIMEOUT      2
+#define BUFFER_SIZE 256
+#define BACKLOG 5
 
 void handle_client(int client_socket) {
     char buffer[BUFFER_SIZE];
@@ -32,19 +30,13 @@ void handle_client(int client_socket) {
         exit(1);
     }
 
-    // if(strcmp(buffer, "End Socket Connection") == 0) {
-    //     printf("Closing connection as per client request \n");
-    //     close(client_socket);
-    // }
     close(client_socket);
 }
 
 int main(int argc, char *argv[]) {
-    int server_socket;
-    int client_socket;
+    int server_socket, client_socket;
     struct sockaddr_un server_addr;
-    ssize_t numRead;
-    char buf[BUFFER_SIZE];
+    ssize_t numRead; 
 
     // If there are socket file which has same name already
     if(access(SOCKET_PATH, F_OK) == 0) {
@@ -59,7 +51,7 @@ int main(int argc, char *argv[]) {
 
     memset(&server_addr, 0, sizeof(struct sockaddr_un));
     server_addr.sun_family = AF_UNIX;
-    snprintf(server_addr.sun_path, sizeof(server_addr.sun_path), "%s", SOCKET_PATH);
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
 
     if(bind(server_socket, (struct sockaddr *) &server_addr, sizeof(struct sockaddr_un)) < 0) {
         perror("ERROR on binding");
@@ -75,22 +67,22 @@ int main(int argc, char *argv[]) {
 
     while(1) {
         client_socket = accept(server_socket, NULL, NULL);
-        if(client_socket < 0) { 
+        if(client_socket < 0) {
             perror("ERROR on accept");
             exit(1);
         }
 
-        // while((numRead= read(client_socket, buf, BUFFER_SIZE)) > 0) {
-        //     if(write(STDOUT_FILENO, buf, numRead) != numRead) {
-        //         break;
-        //     }
-        // }
-
-        if(numRead < 0) {
-            perror("ERROR on reading");
+        while((numRead= read(cfd, buf, BUFFER_SIZE)) > 0) {
+            if(write(STDOUT_FILENO, buf, numRead) != numRead) {
+                break;
+            }
         }
 
-        handle_client(client_socket);
+        if(numRead < 0) {
+            perror("ERROR on reading")
+        }
+
+        // handle_client(client_socket);
     }
 
     printf("Server is unconected on %s\n", SOCKET_PATH);
