1014284cefea7a477a19e98210a5242615881041
Yg-Hong <lion0077v@gmail.com>
2024-08-13 10:19:20 +0900

    complete

--- server.c
+++ server.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h>
+#include <sys/socket.h> 
 #include <sys/un.h>
 #include <unistd.h>
 
--- test/src/main/java/com/example/test/SocketProvider.java
+++ test/src/main/java/com/example/test/SocketProvider.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.URI;
 
